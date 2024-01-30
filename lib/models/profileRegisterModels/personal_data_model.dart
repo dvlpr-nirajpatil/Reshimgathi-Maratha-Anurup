f57fa31576159189fb6b9321692c843b95965bb4
dvlpr-nirajpatil <dev.nirajpatil@gmail.com>
2024-01-30 18:30:07 +0530

    create personal data model

---
+++ lib/models/profileRegisterModels/personal_data_model.dart
@@ -0,0 +1,19 @@
+import 'package:reshimgathi/consts/consts.dart';
+
+class PersonalDataModel {
+  TextEditingController firstName = TextEditingController();
+  TextEditingController lastName = TextEditingController();
+  TextEditingController birthDate = TextEditingController();
+  TextEditingController birthTime = TextEditingController();
+  TextEditingController birthPlace = TextEditingController();
+  TextEditingController birthName = TextEditingController();
+  TextEditingController heightInFeet = TextEditingController();
+  TextEditingController heightInInches = TextEditingController();
+  TextEditingController physicalDisability = TextEditingController();
+  String? gender;
+  String? ras;
+  String? bloodGroup;
+  String? maritalStatus;
+  String? is_physicallyDissabled;
+  String? caste;
+}
